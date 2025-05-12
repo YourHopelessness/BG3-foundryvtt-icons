d5929458d3021e70d775438ea8d225034cb5ff40
Дарья <jiraffica@gmail.com>
2025-05-12 20:45:06 +0300

    added all icons

--- main.py
+++ main.py
@@ -29,24 +29,35 @@ CATEGORIES_URLS = {
     "bg3_spell_icons": f"{BASE_URL}/wiki/Category:Spell_tooltip_images",
 }
 
-def get_image_links(page_url):
-    response = requests.get(page_url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    image_links = []
+def get_all_image_links(category_url):
     headers = {"User-Agent": "Mozilla/5.0"}
+    image_links = []
+    next_url = category_url
 
-    for li in soup.select("div.mw-content-ltr ul li"):
-        link = li.find("a")
-        if link and link.get("href"):
-            file_page_url = urljoin(BASE_URL, link["href"])
-            file_response = requests.get(file_page_url, headers=headers)
-            file_soup = BeautifulSoup(file_response.text, "html.parser")
-            
-            img = file_soup.find("img", src=lambda s: s and "/w/images/" in s)
-            if img:
-                image_url = urljoin(BASE_URL, img["src"])
-                image_links.append(image_url)
+    while next_url:
+        print(f"Обработка страницы: {next_url}")
+        response = requests.get(next_url, headers=headers)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        for li in soup.select("div.mw-content-ltr ul li"):
+            link = li.find("a")
+            if link and link.get("href"):
+                file_page_url = urljoin(BASE_URL, link["href"])
+                file_response = requests.get(file_page_url, headers=headers)
+                file_soup = BeautifulSoup(file_response.text, "html.parser")
                 
+                img = file_soup.find("img", src=lambda s: s and "/w/images/" in s)
+                if img:
+                    image_url = urljoin(BASE_URL, img["src"])
+                    image_links.append(image_url)
+
+        # Поиск ссылки на следующую страницу
+        next_link = soup.find("a", string="next page")
+        if next_link:
+            next_url = urljoin(BASE_URL, next_link["href"])
+        else:
+            next_url = None  # Конец
+
     return image_links
 
 def download_images(path, image_urls):
@@ -54,7 +65,6 @@ def download_images(path, image_urls):
         filename = safe_filename(url)
         filepath = os.path.join(path, filename)
         if not os.path.exists(filepath):
-            print(f"Скачивание {filename}...")
             img_data = requests.get(url).content
             with open(filepath, "wb") as f:
                 f.write(img_data)
@@ -64,8 +74,8 @@ if __name__ == "__main__":
     for path, url in CATEGORIES_URLS.items():
         os.makedirs(path, exist_ok=True)
         
-        print(f"Обработка страницы: {url}")
-        image_links = get_image_links(url)
+        image_links = get_all_image_links(url)
+        print("Найдено изображений:", len(image_links))
         all_image_links += len(image_links)
         download_images(path, image_links)
     print(f"Загрузка завершена. Загружено {all_image_links}")
